9f3d80aa20d3cdc850b4eb3260e6f496c85ff110
Casta Mo <502442134@qq.com>
2017-05-17 16:25:04 +0800

    接口打通

--- main/common/index.js
+++ main/common/index.js
@@ -1,10 +1,38 @@
 var spawnSync = require("child_process").spawnSync;
+var spawn = require("child_process").spawn;
 
-exports.parseAndPrint = function parseAndPrint() {
+
+exports.parseAndPrint = function parseAndPrint(options, callback) {
 	console.log("start parse");
-	spawnSync("phantomjs", ["common/p.js"]);
-	console.log("parse end!\nstart print");
-	spawnSync("gsprint", ["test.pdf"]);
-	console.log("print end!");
+
+
+	var pjs_args = [
+		"common/p.js",
+		options.h5,
+		options.me
+	];
+
+	console.log(pjs_args);
+
+	var pjs = spawn("phantomjs", pjs_args);
+
+	pjs.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	pjs.stderr.on('data', (data) => {
+		console.log(`stderr: ${data}`);
+	});
+
+	pjs.on("close", function() {
+		console.log("parse end!\nstart print");
+		var print = spawn("gsprint", ["test.pdf"]);
+		print.on("close", function() {
+			console.log("print end!");
+			if (typeof callback === "function") {
+				callback(null);
+			}
+		});
+	});
 }
 
--- main/common/p.js
+++ main/common/p.js
@@ -1,8 +1,6 @@
 var page = require('webpage').create();
 
-
-// var express = require('express');
-
+var system = require('system');
 // var port = 3000;
 // var app = express();
 // app.use(function(req, res, next) {
@@ -18,19 +16,42 @@ var page = require('webpage').create();
 //     console.log('Listening at ' + uri + '\n')
 // })
 
-
 var template_path = "http://localhost:8080/main";
+
+console.log("ggggg");
+var args = system.args;
+// var args = process.argv;
+
+
+var h5 = args[1],
+	me = args[2];
+
+// var h5 = "http://localhost:8888/qrcode/get?dest=http://www.baidu.com",
+// 	me = "http://threearse-1252859479.picgz.myqcloud.com/test/2.jpeg?imageView2";
 
 template_path = "http://localhost:8888/template/result";
 //template_path = "file:///C:/Users/binl/Desktop/printer/hw2-table-sorter-master/13331202_%E8%8E%AB%E5%86%A0%E9%92%8A/index.html";
 
-page.open(template_path, function() {
+var querys = [
+	"h5=" + h5,
+	"me=" + me
+];
+
+var result = [
+	template_path,
+	"?",
+	querys.join("&")
+];
+
+var url = result.join("");
+console.log(url);
+page.open(url, function() {
 	page.render('test.pdf', {
 		format: 'pdf',
 		quality: '100'
 	});
-  console.log("gg");
-  phantom.exit();
+	console.log("gg");
+	phantom.exit();
 });
 
 	
