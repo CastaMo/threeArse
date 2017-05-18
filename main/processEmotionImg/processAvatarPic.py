e20504282aa0a31ca962177d9ef9da66033b44fd
Casta Mo <502442134@qq.com>
2017-05-19 06:14:59 +0800

    完成avatar处理的脚本逻辑

---
+++ main/processEmotionImg/processAvatarPic.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+from PIL import Image
+from PIL import ImageFont, ImageDraw
+from time import gmtime, strftime
+import sys
+
+originImg = sys.argv[1]
+saveImg = sys.argv[2]
+
+img = Image.open(originImg)
+
+img = img.convert('1')
+
+img.save(saveImg)
